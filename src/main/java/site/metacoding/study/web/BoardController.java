8718222c2625e226361d3b8401f9abdf977d760b
LV <whgodbs32191@gmail.com>
2022-04-14 18:16:06 +0900

    게시판 디자인 완료

--- src/main/java/site/metacoding/study/web/BoardController.java
+++ src/main/java/site/metacoding/study/web/BoardController.java
@@ -1,6 +1,8 @@
 package site.metacoding.study.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.study.domain.BoardRepository;
@@ -10,4 +12,26 @@ import site.metacoding.study.domain.BoardRepository;
 public class BoardController {
 
     private final BoardRepository boardRepository;
+
+    @GetMapping({ "/", "/board" })
+    public String boardList() {
+        return "boardList";
+
+    }
+
+    @GetMapping("/board/{id}")
+    public String boardDetail(@PathVariable Integer id) {
+        return "boardDetail";
+
+    }
+
+    @GetMapping("/board/writeForm")
+    public String boardWriteForm() {
+        return "boardWriteForm";
+    }
+
+    @GetMapping("/board/updateForm")
+    public String boardUpdateForm() {
+        return "boardUpdateForm";
+    }
 }
